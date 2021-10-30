d8fc929546057e5e5dbcc8c68c401527dbf2b722
surajguptacubexo <soorajguptamanpur@gmail.com>
2021-10-30 16:50:09 -0700

    NoteApp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NoteList from './components/NoteList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <NoteList />
+    </div>
+  );
+}
+
+export default App;
